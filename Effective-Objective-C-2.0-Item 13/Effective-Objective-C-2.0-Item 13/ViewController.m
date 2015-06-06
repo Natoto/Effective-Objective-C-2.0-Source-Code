094959ba5ff114faf905b1ef2002b9f681ccca55
黄成都 <394042583@qq.com>
2015-06-06 16:57:24 +0800

    init

---
+++ Effective-Objective-C-2.0-Item 13/Effective-Objective-C-2.0-Item 13/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Effective-Objective-C-2.0-Item 13
+//
+//  Created by maiyun on 15/6/6.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
