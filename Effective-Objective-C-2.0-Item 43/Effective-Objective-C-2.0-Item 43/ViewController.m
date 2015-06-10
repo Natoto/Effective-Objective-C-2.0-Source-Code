f2c235024efdcf6fef1fd8b1429511731011cad5
huang chengdu <huang_303513@163.com>
2015-06-11 07:03:57 +0800

    第43个代码实现

---
+++ Effective-Objective-C-2.0-Item 43/Effective-Objective-C-2.0-Item 43/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  Effective-Objective-C-2.0-Item 43
+//
+//  Created by 黄成都 on 15/6/11.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+@end
