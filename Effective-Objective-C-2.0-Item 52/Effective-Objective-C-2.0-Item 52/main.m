8ceafa9fbc3896ac0d773d57b6682410732bb5a0
黄成都 <394042583@qq.com>
2015-06-11 17:49:45 +0800

    d

---
+++ Effective-Objective-C-2.0-Item 52/Effective-Objective-C-2.0-Item 52/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Effective-Objective-C-2.0-Item 52
+//
+//  Created by maiyun on 15/6/11.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
